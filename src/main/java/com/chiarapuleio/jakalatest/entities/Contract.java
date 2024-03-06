97570f51e79187753c259838ccfdf8503a78f9ea
Chiara Puleio <chiarapuleio@yahoo.it>
2024-03-06 12:21:08 +0100

    changes on Contract class to start DB

--- src/main/java/com/chiarapuleio/jakalatest/entities/Contract.java
+++ src/main/java/com/chiarapuleio/jakalatest/entities/Contract.java
@@ -1,10 +1,16 @@
 package com.chiarapuleio.jakalatest.entities;
 
 import com.chiarapuleio.jakalatest.entities.abstractClass.Customer;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 
+import java.util.UUID;
+
+@Entity
+@Table(name = "contracts")
 public class Contract {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
     @ManyToOne
     @JoinColumn(name = "customer_id")
     private Customer customer;
