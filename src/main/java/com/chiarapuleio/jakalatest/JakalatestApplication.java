ec0debd3b5f69e037168dd92d0435a5cc93cb2bc
Chiara Puleio <chiarapuleio@yahoo.it>
2024-03-06 10:28:48 +0100

    Initial commit, empty project

---
+++ src/main/java/com/chiarapuleio/jakalatest/JakalatestApplication.java
@@ -0,0 +1,13 @@
+package com.chiarapuleio.jakalatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JakalatestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JakalatestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chiarapuleio/jakalatest/JakalatestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chiarapuleio.jakalatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JakalatestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
